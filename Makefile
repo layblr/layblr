037384532b5ed285b917bdfb2a37bc92c55b055b
Tom Valk <tomvalk@gmail.com>
2019-04-08 16:21:46 +0300

    [TASK] Add setup.py and manifest files.

---
+++ Makefile
@@ -0,0 +1,19 @@
+
+build:
+	@ python setup.py sdist
+#	@ python setup.py bdist_wheel --python-tag py3
+
+publish:
+	@ python setup.py sdist
+#	@ python setup.py bdist_wheel --python-tag py3
+	@ twine upload dist/audiostamper-*
+
+run-tox:
+	@ tox
+
+tests: run-tox clean
+
+clean:
+	@ find . -name '*.py[co]' -delete
+	@ find . -name '__pycache__' -delete
+	@ rm -rf *.egg-info dist/audiostamper-* build
---
+++ setup.py
@@ -0,0 +1,79 @@
+import os
+import re
+from setuptools import setup, find_packages
+
+
+def long_description():
+	try:
+		return open(os.path.join(os.path.dirname(__file__), 'README.md')).read()
+	except IOError:
+		return None
+
+
+def read_version():
+	with open(os.path.join(os.path.dirname(__file__), 'audiostamper', '__init__.py')) as handler:
+		return re.search(r"^__version__ = ['\"]([^'\"]*)['\"]", handler.read(), re.M).group(1)
+
+
+def read_requirements(filename):
+	with open(os.path.join(os.path.dirname(__file__), filename), 'r') as handler:
+		return [line for line in handler.readlines() if not line.startswith('#') and not line.startswith('-') and not len(line) <= 1]
+
+
+EXCLUDE_FROM_PACKAGES = [
+	'docs*',
+	'env*',
+	'tests*',
+	'src*',
+	'e2e*',
+	'node_modules*',
+]
+
+PKG = 'audiostamper'
+######
+setup(
+	name=PKG,
+	version=read_version(),
+	description='Browser based audio feature tagger',
+	long_description=long_description(),
+	keywords='maniaplanet, controller, plugins, apps',
+	license='GNU General Public License v3 (GPLv3)',
+	packages=find_packages(exclude=EXCLUDE_FROM_PACKAGES),
+	package_data={
+		'audiostamper': [
+			'dist/**'
+		]
+	},
+	install_requires=read_requirements('requirements.txt'),
+	tests_require=read_requirements('requirements-dev.txt'),
+	extras_require={},
+	# test_suite='tests',
+	include_package_data=True,
+
+	scripts=['audiostamper/bin/audiostamper'],
+	entry_points={'console_scripts': [
+		'audiostamper = audiostamper.stamp:execute_from_cli',
+	]},
+
+	author='Tom Valk',
+	author_email='tomvalk@lt-box.info',
+
+	classifiers=[
+		'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+		'Development Status :: 4 - Beta',
+
+		'Programming Language :: Python :: 3',
+		'Programming Language :: Python :: 3.6',
+		'Programming Language :: Python :: 3.7',
+		'Programming Language :: Python :: 3 :: Only',
+
+		'Operating System :: OS Independent',
+
+		'Topic :: Internet',
+		'Topic :: Software Development :: Libraries :: Python Modules',
+
+		'Intended Audience :: Developers',
+
+	],
+	zip_safe=False,
+)
